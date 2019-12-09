f1fa271626b82069cb018dac3aeba1dadf9da90f
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-12-09 14:58:19 +0100

    wrap in memo

--- packages/maker/src/renderer/components/check-input/CheckInput.tsx
+++ packages/maker/src/renderer/components/check-input/CheckInput.tsx
@@ -3,16 +3,23 @@ import { Field } from 'formik';
 import cx from 'classnames';
 export const CheckInput: React.FC<React.HTMLProps<HTMLInputElement> & {
   type: 'radio' | 'checkbox';
-  label: string;
+  label?: string;
   className?: string;
-}> = props => {
-  const { current: id } = React.useRef<string>(props.id || `${Math.random()}`);
+}> = React.memo(props => {
+  const [id, setId] = React.useState(props.id);
+  React.useEffect(() => {
+    if (!props.id) {
+      setId(`CheckInput_${Math.floor(Math.random() * 10000)}`);
+    }
+  }, []);
+
   return (
     <>
       <Field {...props} id={id} className={cx('is-checkradio', props.className)} />
+
       <label htmlFor={id} className={props.type}>
         {props.label}
       </label>
     </>
   );
-};
+});
