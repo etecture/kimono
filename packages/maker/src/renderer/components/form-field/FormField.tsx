a58438d41bdbcfb51fde786283b9da448155b0c1
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-12-09 14:59:30 +0100

    support horizontal prop

--- packages/maker/src/renderer/components/form-field/FormField.tsx
+++ packages/maker/src/renderer/components/form-field/FormField.tsx
@@ -1,59 +1,58 @@
 import React, { ReactElement } from 'react';
 import cx from 'classnames';
+import styled from 'styled-components';
 
-export const FormField: React.FC<{
+export interface FormFieldProps {
   label?: string;
   className?: string;
-  error?: string;
+  error?: string | string[];
   addonBefore?: string;
   addonAfter?: string;
-}> = props => {
-  return (
-    <div className={cx('field', props.className)}>
-      <label className="label">{props.label}</label>
-      <div className={cx('field', { 'has-addons': props.addonBefore || props.addonAfter })}>
-        {props.addonBefore && (
-          <div className="control">
-            <div className="button is-static">{props.addonBefore}</div>
-          </div>
-        )}
-        <div className="control is-flex is-expanded">
-          {React.Children.map(props.children, c => {
-            const child = c as ReactElement;
-            return React.cloneElement(child, {
-              ...child.props,
-              className: cx({ 'is-danger': props.error }, child.props.className)
-            });
-          })}
-        </div>
-        {props.addonAfter && (
-          <div className="control">
-            <div className="button is-static">{props.addonAfter}</div>
-          </div>
-        )}
-        {props.error ? <div className="help is-danger">{props.error}</div> : null}
-      </div>
-    </div>
-  );
-};
-/* 
+  horizontal?: boolean;
+}
 
-<div className="field is-horizontal">
-  <div class="field-label"></div>
-  <div class="field-body">
-    <div class="field is-expanded">
-      <div class="field has-addons">
-        <p class="control">
-          <a class="button is-static">
-            +44
-          </a>
-        </p>
-        <p class="control is-expanded">
-          <input class="input" type="tel" placeholder="Your phone number">
-        </p>
+const StyledFormField = styled.div`
+  &.is-horizontal {
+    @media screen and (min-width: 769px), print {
+      .field-label {
+        padding-top: 0.5rem;
+      }
+    }
+  }
+`;
+
+export const FormField: React.FC<FormFieldProps> = React.memo(props => {
+  return (
+    <StyledFormField className={cx('field', props.className, { 'is-horizontal': props.horizontal })}>
+      <label className={cx('label', { 'field-label': props.horizontal })}>{props.label}</label>
+      <div className={cx({ 'field-body': props.horizontal })}>
+        <div className={cx('field', { 'has-addons': props.addonBefore || props.addonAfter })}>
+          {props.addonBefore && (
+            <div className="control">
+              <div className="button is-static">{props.addonBefore}</div>
+            </div>
+          )}
+          <div className={'control is-flex'}>
+            {React.Children.map(props.children, c => {
+              const child = c as ReactElement;
+              return React.cloneElement(child, {
+                ...child.props,
+                className: cx({ 'is-danger': props.error }, child.props.className)
+              });
+            })}
+          </div>
+          {props.addonAfter && (
+            <div className="control">
+              <div className="button is-static">{props.addonAfter}</div>
+            </div>
+          )}
+          {props.error ? (
+            <div className="help is-danger">
+              {Array.isArray(props.error) ? props.error.map((err, idx) => <div key={idx}>{err}</div>) : props.error}
+            </div>
+          ) : null}
+        </div>
       </div>
-      <p class="help">Do not enter the first zero</p>
-    </div>
-  </div>
-</div>
- */
+    </StyledFormField>
+  );
+});
