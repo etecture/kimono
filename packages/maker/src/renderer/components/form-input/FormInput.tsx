9cfa31fbdaccbffde04e2db19e7b162a2c021968
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-12-09 14:59:48 +0100

    support clearable prop

--- packages/maker/src/renderer/components/form-input/FormInput.tsx
+++ packages/maker/src/renderer/components/form-input/FormInput.tsx
@@ -1,6 +1,43 @@
 import React from 'react';
-import { Field } from 'formik';
+import { Field, FieldProps } from 'formik';
 import cx from 'classnames';
+import { Icon } from '../icon/Icon';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  border: 0;
+  box-shadow: none;
+  position: absolute;
+  top: 2px;
+  right: 2px;
+  bottom: 2px;
+  height: auto;
+`;
+
 export const Input: React.FC<React.HTMLProps<HTMLInputElement> & {
   className?: string;
-}> = props => <Field {...props} className={cx('input', props.className)} />;
+  clearable?: boolean;
+}> = React.memo(({ clearable, ...props }) => {
+  const inputRef = React.useRef<HTMLInputElement | null>(null);
+  return (
+    <Field {...props}>
+      {({ field, form }: FieldProps) => (
+        <>
+          <input ref={inputRef} {...field} className={cx('input', props.className)} />
+          {!!(clearable && field.value) && (
+            <StyledButton
+              className="button"
+              type="button"
+              onClick={() => {
+                form.setFieldValue(field.name, '');
+                inputRef.current?.focus();
+              }}
+            >
+              <Icon icon="fa-times" />
+            </StyledButton>
+          )}
+        </>
+      )}
+    </Field>
+  );
+});
