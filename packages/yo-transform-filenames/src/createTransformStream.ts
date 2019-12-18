28669b9bf25711ccc660d45b07ffd853f2313e76
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-12-18 07:55:19 +0100

    feat(yo-utils): add support for ignoring files from EJS parsing in the transform stream

--- packages/yo-transform-filenames/src/createTransformStream.ts
+++ packages/yo-transform-filenames/src/createTransformStream.ts
@@ -1,11 +1,24 @@
 import gulpRename, { ParsedPath } from 'gulp-rename';
-
+import { fsUtils } from '@kimono/yo-utils';
 import { renderPath } from './renderPath';
 import { getFilePath } from './utils';
 
-export function createTransformStream(context: {}): NodeJS.ReadWriteStream {
+/**
+ * Creates a stream for the yo generator which will parse and render the filenames of template
+ * files and folders using our adopted EJS filesystem syntax.
+ *
+ * @param context An object with key/value pairs that will be available as template variables
+ * @param ignored An array of filenames to exclude from EJS parsing
+ */
+export function createTransformStream(context: {}, ignored?: string[]): NodeJS.ReadWriteStream {
   return gulpRename((path: ParsedPath) => {
-    const filepath: string = getFilePath(path);
-    Object.assign(path, renderPath(filepath, context));
+    const currentFile: string = fsUtils.forwardSlashes(getFilePath(path));
+
+    if (ignored && ignored.some(ignoredFile => currentFile === fsUtils.forwardSlashes(ignoredFile))) {
+      return;
+    }
+
+    const renderedPath = renderPath(getFilePath(path), context);
+    Object.assign(path, renderedPath);
   });
 }
