cfb703fd1e9270a49a88df752ced7c08bfb58d35
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-12-18 07:58:53 +0100

    docs: adjust wording

--- packages/yo-transform-filenames/src/createTransformStream.ts
+++ packages/yo-transform-filenames/src/createTransformStream.ts
@@ -7,7 +7,7 @@ import { getFilePath } from './utils';
  * Creates a stream for the yo generator which will parse and render the filenames of template
  * files and folders using our adopted EJS filesystem syntax.
  *
- * @param context An object with key/value pairs that will be available as template variables
+ * @param context An object with key/value pairs that will be used as template variables
  * @param ignored An array of filenames to exclude from EJS parsing
  */
 export function createTransformStream(context: {}, ignored?: string[]): NodeJS.ReadWriteStream {
