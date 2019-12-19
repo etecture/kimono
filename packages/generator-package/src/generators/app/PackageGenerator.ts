246f6dfa63c7dbed9320732d98bffceab2ef16a2
Jovica Aleksic <jovica.aleksic@etecture.de>
2019-12-19 10:01:27 +0100

    feat(generator-package): support git-based --tpl feat(generator-package): support kimono.json in templates feat(generator-package): support nested templates with noparse field in kimono.json

--- packages/generator-package/src/generators/app/PackageGenerator.ts
+++ packages/generator-package/src/generators/app/PackageGenerator.ts
@@ -6,18 +6,22 @@ import yosay from 'yosay';
 import symlinkDir from 'symlink-dir';
 
 import { createTransformStream, createIgnoreGlobs, getFilePath, renderPath } from '@kimono/yo-transform-filenames';
-import { binaryUtils, questionUtils, packageUtils, fsUtils } from '@kimono/yo-utils';
-
+import { binaryUtils, questionUtils, packageUtils, fsUtils, shellUtils } from '@kimono/yo-utils';
 import { options as appOptions } from './options';
 import { PackageGeneratorOptions } from '../../types';
 
 import { doneMessage } from '../../doneMessage';
+
+import { TemplateOptions } from './TemplateOptions';
 const { name, version } = require('../../../package.json');
 
 export default class PackageGenerator extends Generator {
   templateVars?: PackageGeneratorOptions;
   // cwd gets changed during the process. we remember the initial value.
   initialCwd: string;
+  // files and folders we create and need to remove afterwards
+  temporaryFiles: string[] = [];
+
   constructor(args: string | string[], opts: {}) {
     super(args, opts);
     this.initialCwd = process.cwd();
@@ -50,60 +54,62 @@ export default class PackageGenerator extends Generator {
       process.exit(1);
     }
 
-    if (this.options.scope) {
-      // special override via --scope
-      this.templateVars.packageScope = this.options.scope;
-    }
-
-    this.templateVars = this._finalizeContext(this.templateVars!);
-    if (this.templateVars.dest) {
-      fs.ensureDirSync(path.resolve(this.templateVars.dest));
-    }
+    this.templateVars = this._normalizeOptions(this.templateVars!);
 
     this.destinationRoot(path.resolve(this.templateVars.dest || '', this.templateVars.packageName!));
   }
 
-  _finalizeContext(context: PackageGeneratorOptions): PackageGeneratorOptions {
+  _normalizeName(context: PackageGeneratorOptions): PackageGeneratorOptions {
     let result = { ...context };
 
-    result = questionUtils.applyImplicitOptions(result);
-
-    if (result.packageScope) {
-      // explicit scope override
+    // apply explicit scope override (when user provided a --scope option)
+    if (this.options.scope) {
+      const packageScope = this.options.scope;
       const { packageName } = packageUtils.splitName(result.projectName!);
-      const packageScope = result.packageScope;
       result.projectName = packageUtils.joinName({ packageScope, packageName });
     }
 
+    // normalize projectScope, projectName and packageName
     result.projectName = packageUtils.normalizePackageName(result.projectName!);
     const { packageName, packageScope } = packageUtils.splitName(result.projectName!);
     result.packageName = packageName;
     result.packageScope = packageScope || '';
 
-    // some values must be computed or derived based on the given values
-    // TODO make this controllable, e.g. option with default
-    const variantFields = ['projectName', 'packageName', 'packageScope'];
-    result = questionUtils.addCamelCased(result, variantFields);
-    result = questionUtils.addPascalCased(result, variantFields);
+    // add case variants for the fields
+    const fields = ['projectName', 'packageName', 'packageScope'];
+    result = questionUtils.addCamelCased(result, fields);
+    result = questionUtils.addPascalCased(result, fields);
+    return result;
+  }
 
-    // package.json repository
+  _normalizeOptions(context: PackageGeneratorOptions): PackageGeneratorOptions {
+    let result = { ...context };
+
+    result = questionUtils.applyImplicitOptions(result);
+    result = this._normalizeName(result);
+
+    // package.json "repository" field
     if (result.repositoryType || result.repositoryUrl) {
       result.repository = packageUtils.shake({ type: result.repositoryType, url: result.repositoryUrl });
     } else {
+      result.repositoryType = '';
+      result.repositoryUrl = '';
       result.repository = null;
     }
 
-    // package.json publishConfig
+    // package.json "publishConfig" field
     if (result.publishAccess || result.publishRegistry) {
       result.publishConfig = packageUtils.shake({
         access: result.publishAccess,
         registry: result.publishRegistry
       });
     } else {
+      result.publishAccess = '';
+      result.publishRegistry = '';
       result.publishConfig = null;
     }
 
-    // package.json dependencies
+    // package.json dependencies fields
     if (result.private === undefined) result.private = false;
     if (!result.dependencies) result.dependencies = '';
     if (!result.devDependencies) result.devDependencies = '';
@@ -122,30 +128,69 @@ export default class PackageGenerator extends Generator {
     if (result.tpl) {
       if (result.tpl.startsWith('.')) {
         result.tpl = path.resolve(process.cwd(), result.tpl);
-      } else {
+      } else if (!result.tpl.endsWith('.git')) {
         result.tpl = path.resolve(result.tpl);
       }
     }
 
     return result;
   }
+
   async writing() {
+    //--------------------------------------------------------------------
+    // RESOLVE TEMPLATE FILES
+    // The user may have provided a custom template location via --tpl
+    // In case the --tpl value points to a git repo, we need to clone it
+    //--------------------------------------------------------------------
     if (this.templateVars!.tpl) {
-      this.sourceRoot(this.templateVars!.tpl);
+      if (this.templateVars!.tpl?.endsWith('.git')) {
+        const tempDir = shellUtils.createTempDir(this.templateVars!.tpl);
+        this.temporaryFiles.push(tempDir);
+        await shellUtils.run(`cd ${tempDir} && git clone ${this.templateVars!.tpl} .`);
+        this.sourceRoot(tempDir);
+      } else {
+        this.sourceRoot(this.templateVars!.tpl);
+      }
     }
+    if (this.templateVars!.dest) {
+      fs.ensureDirSync(path.resolve(this.templateVars!.dest));
+    }
+
     const context = this.templateVars as {};
+
+    //--------------------------------------------------------------------
+    // TEMPLATE CONFIG
+    // The used template may or may not provide a kimono.json file with options
+    // Regardless of whether it does, tplConfig will always be at least an empty object
+    //--------------------------------------------------------------------
+    const tplConfig: TemplateOptions = (await fsUtils.loadJSON(this.templatePath('kimono.json'))) || {};
+
+    const ignoredGitFiles = [
+      ...fsUtils.getFilesRecursively(this.templatePath('.git')),
+      ...(await fsUtils.resolveGitIgnoredFiles(this.templatePath('.gitignore')))
+    ];
+
+    //--------------------------------------------------------------------
+    // NESTED TEMPLATE FILES
+    // The template can include templates itself (for providing template-based functionality to the end-user).
+    // We shouldn't parse the EJS syntax in those "sub-template" files just yet.
+    // The template may provide a "noparse" array of globs for those files, which we resolve to actual filenames
+    //--------------------------------------------------------------------
+    const ignoredSubTemplates = await fsUtils.resolveGlobs(tplConfig.noparse, { cwd: this.templatePath() });
+
     //--------------------------------------------------------------------
     // FILENAME TEMPLATE SYNTAX
-    // - render EJS syntax in filenames using a transform stream
-    // - create array of skipped files that have falsy conditional names
+    // - render custom EJS syntax in filenames using a transform stream
+    // - create array of falsy file names after rendering (the files will be omitted)
     //--------------------------------------------------------------------
-    this.registerTransformStream(createTransformStream(context));
+    this.registerTransformStream(createTransformStream(context, [...ignoredGitFiles, ...ignoredSubTemplates]));
     const ignoredConditionalFiles = (await createIgnoreGlobs(this.templatePath(), context)) as Set<string>;
 
     //--------------------------------------------------------------------
     // BINARY TEMPLATE FILES
-    // template files that are binary throw errors when copied via copyTpl
-    // we exclude them from fs.copyTpl and manually copy them instead
+    // template files that are binary (e.g. images) throw errors when they
+    // are copied via copyTpl, because copyTpl tries to parse them as EJS text.
+    // Therefore, we exclude them from fs.copyTpl and instead manually copy them later
     //--------------------------------------------------------------------
     const binaryTemplateFiles = binaryUtils.getBinaryFiles(this.templatePath(), ignoredConditionalFiles);
     binaryTemplateFiles.forEach((file: string) => {
@@ -159,7 +204,7 @@ export default class PackageGenerator extends Generator {
     const ignoredBinaryFiles = binaryUtils.getBinaryIgnoreGlobs(binaryTemplateFiles);
 
     //--------------------------------------------------------------------
-    // COPY REGULAR TEMPLATE FILES
+    // COPY THE REGULAR TEMPLATE FILES
     //--------------------------------------------------------------------
 
     this.fs.copyTpl(
@@ -170,7 +215,7 @@ export default class PackageGenerator extends Generator {
       {
         globOptions: {
           dot: true,
-          ignore: [...ignoredBinaryFiles, ...ignoredConditionalFiles]
+          ignore: [...ignoredBinaryFiles, ...ignoredConditionalFiles, ...ignoredSubTemplates, ...ignoredGitFiles]
         }
       }
     );
@@ -201,14 +246,16 @@ export default class PackageGenerator extends Generator {
   _formatPackageJson() {
     const filePath = path.resolve(this.destinationPath('package.json'));
     if (fs.pathExistsSync(filePath)) {
-      // re-format package.json
       fs.writeFileSync(filePath, JSON.stringify(require(filePath), null, 2));
-    } else {
-      console.warn('>> package.json not found in output directory!', filePath);
     }
   }
 
-  end() {
+  async end() {
+    for (const temp of this.temporaryFiles) {
+      await shellUtils.run(`rm -rf ${temp}`);
+    }
+    this.temporaryFiles.length = 0;
+
     this._formatPackageJson();
 
     const messages = [
---
+++ packages/generator-package/src/generators/app/TemplateOptions.ts
@@ -0,0 +1,3 @@
+export interface TemplateOptions {
+  noparse?: string[];
+}
